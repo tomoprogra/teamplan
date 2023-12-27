0f0417aacc3f6dde375a5f307786e47e7dfebe1c
tomoprogra <anyonewww@outlook.jp>
2023-12-27 21:25:49 +0900

    修正

--- entrypoint.sh
+++ entrypoint.sh
@@ -4,11 +4,13 @@ set -e
 
 rm -f /test_app/tmp/pids/server.pid
 
+if [ "$RAILS_ENV" = "production" ]; then
+  echo "Production environment detected. Running production-specific tasks..."
 
-bundle install
-bundle exec rails assets:clobber
-bundle exec rails assets:precompile
-bundle exec rails db:migrate
-
+  bundle install --without development test
+  bundle exec rails assets:clobber
+  bundle exec rails assets:precompile
+  bundle exec rails db:migrate
+fi
 
 exec "$@"
