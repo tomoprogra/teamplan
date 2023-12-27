206eae72ced1307b4c211219c7d36f08ab2158c0
tomoprogra <anyonewww@outlook.jp>
2023-12-27 09:47:03 +0900

    修正

--- Dockerfile
+++ Dockerfile
@@ -30,4 +30,6 @@ RUN chmod +x /usr/bin/entrypoint.sh
 
 ENTRYPOINT ["entrypoint.sh"]
 
-CMD ["./bin/dev"]
+EXPOSE 3000
+
+CMD [ "rails","server","-b","0.0.0.0" ]
--- entrypoint.sh
+++ entrypoint.sh
@@ -4,12 +4,11 @@ set -e
 
 rm -f /test_app/tmp/pids/server.pid
 
-if [ "$RAILS_ENV" = "production" ]; then
+
 bundle install
 bundle exec rails assets:clobber
 bundle exec rails assets:precompile
 bundle exec rails db:migrate
-fi
+
 
 exec "$@"
-bundle exec rails s -p ${PORT:-3000} -b 0.0.0.0
