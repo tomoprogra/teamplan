d4a8058e812df6f7c8d32923f35b6572783aa735
tomoprogra <anyonewww@outlook.jp>
2024-01-05 18:20:04 +0900

    devise,letter_opener_webの導入

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  devise_for :users
+  mount LetterOpenerWeb::Engine, at: "/letter_opener" if Rails.env.development?
   root "tops#index"
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
