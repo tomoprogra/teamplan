78df4e5882bd25c08ee787d0cb0d98c8877ddced
chapchon819 <l000merci.819@gmail.com>
2024-01-12 01:58:19 +0900

    fix:mailerのメール文

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,9 +1,10 @@
 module ApplicationHelper
     def flash_background_color(key)
         case key.to_sym
-        when :notice then "bg-green-500"
-        when :alert  then "bg-red-500"
-        else "bg-gray-500"
+        when :notice then "bg-green-50"
+        when :success then "bg-green-50"
+        when :alert  then "bg-red-50"
+        else "bg-yellow-50"
         end
       end
 end
