94f70c352604a562d54070989bee9b0c32735315
noguchi-yu <hnrnyk.12c5@gmail.com>
2024-01-11 21:41:55 +0900

    開始日時のバリデーション追加

--- app/models/event.rb
+++ app/models/event.rb
@@ -6,4 +6,9 @@ class Event < ApplicationRecord
   validates :start_datetime, presence: true
   validates :end_datetime, presence: true
   validates :group_id, presence: true
+  validate :date_before_start
+
+  def date_before_start
+    errors.add(:start_time, "は過去の日付を選択できません") if start_time < Date.today
+  end
 end
