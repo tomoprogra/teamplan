f0aa46787a2b3aedd730ebe06a7af657d19d262d
tomoprogra <anyonewww@outlook.jp>
2024-01-15 11:50:25 +0900

    メール本番環境設定

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -80,7 +80,7 @@ Rails.application.configure do
   config.action_mailer.smtp_settings = {
     address:              'smtp.gmail.com',
     port:                 587,
-    domain:               '***.onrender.com',
+    domain:               'teamplan.onrender.com',
     user_name:            ENV['GMAIL_USERNAME'],
     password:             ENV['GMAIL_PASSWORD'],
     authentication:       'plain',
