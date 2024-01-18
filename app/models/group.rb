76e1e7caa7478bff2d32fe4196268155fd02f7c5
chapchon819 <l000merci.819@gmail.com>
2024-01-19 02:56:16 +0900

    add:通知作成メソッド

--- app/models/group.rb
+++ app/models/group.rb
@@ -6,4 +6,25 @@ class Group < ApplicationRecord
   has_many :invitations, dependent: :destroy
   has_many :notifications, dependent: :destroy
   validates :title, presence: true
+
+  def create_notification_join!(current_user)
+    # グループメンバー全員を検索
+    group_users.each do |temp_id|
+      save_notification_join!(current_user, temp_id['user_id'])
+    end
+  end
+
+  def save_notification_join!(current_user, visited_id)
+    # グループ参加は複数人が参加することが考えられるため、複数回通知する
+    notification = current_user.active_notifications.new(
+      group_id: id,
+      visited_id: visited_id,
+      action: 'join'
+    )
+    # 自分へのグループ参加に対しての場合は、通知済みとする
+    if notification.visitor_id == notification.visited_id
+      notification.checked = true
+    end
+    notification.save if notification.valid?
+  end
 end
