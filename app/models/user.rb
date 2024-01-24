f9c05acd48796d0869c2cddcecde3fef6f2aae68
tomoprogra <anyonewww@outlook.jp>
2024-01-24 18:11:15 +0900

    emailバリデーション

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,13 +1,14 @@
 class User < ApplicationRecord
   mount_uploader :profile_image, ProfileImageUploader
   # Include default devise modules. Others available are:
-  # :lockable, :timeoutable, :trackable and :omniauthable
+  # confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
          
 
   validates :name, presence: true, length: { minimum: 2, maximum: 20 }
-
+  TRUE_EMAIL = /[\w]+@[a-z]+.[a-z]+.?[a-z]+/
+  validates :email, presence: true, format: { with: TRUE_EMAIL}
   has_many :group_users, dependent: :destroy
   has_many :groups, through: :group_users
   has_many :chats, dependent: :destroy
