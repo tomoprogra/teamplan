962a09c54b2d8ea41587e95d92f940c29a68f11f
noguchi-yu <hnrnyk.12c5@gmail.com>
2024-01-25 00:36:05 +0900

    イベントバリデーションの追加

--- app/models/event.rb
+++ app/models/event.rb
@@ -6,9 +6,15 @@ class Event < ApplicationRecord
   validates :start_time, presence: true
   validates :end_time, presence: true
   validates :group_id, presence: true
+  validate :date_before_start
   # validate :date_before_start
 
   # def date_before_start
   #   errors.add(:start_time, "は過去の日付を選択できません") if start_time < Date.today
   # end
+  def date_before_start
+    if start_time && end_time && start_time > end_time
+      errors.add(:end_time, "は開始日時よりも後の日時を選択してください")
+    end
+  end
 end
