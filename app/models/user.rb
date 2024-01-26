20cf4153be96f209d3cc7446b6a868c9f08b4e4d
tomoprogra <anyonewww@outlook.jp>
2024-01-26 16:04:33 +0900

    seedコマンド入力

--- app/models/user.rb
+++ app/models/user.rb
@@ -34,7 +34,6 @@ class User < ApplicationRecord
   def self.guest
     find_or_create_by!(email: 'guest@example.com') do |user|
       user.password = SecureRandom.urlsafe_base64
-      user.confirmed_at = Time.now  # Confirmable を使用している場合は必要
       user.name = "ゲスト"
     end
   end
