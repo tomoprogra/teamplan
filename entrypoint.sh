1d0881b2fd1fc4c523091f7da5c0d180c4cca1fd
tomoprogra <anyonewww@outlook.jp>
2024-01-28 18:50:00 +0900

    調整

--- entrypoint.sh
+++ entrypoint.sh
@@ -10,9 +10,9 @@ if [ "$RAILS_ENV" = "production" ]; then
   bundle install --without development test
   bundle exec rails assets:clobber
   bundle exec rails assets:precompile
-  bundle exec rails db:migrate
+  #bundle exec rails db:migrate
   #bundle exec rails db:seed
-  #DISABLE_DATABASE_ENVIRONMENT_CHECK=1 bundle exec rake db:migrate:reset
+  DISABLE_DATABASE_ENVIRONMENT_CHECK=1 bundle exec rake db:migrate:reset
 fi
 
 exec "$@"
